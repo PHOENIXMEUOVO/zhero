8a4e22354fe4ca29ec996ce2459d0f46390f723b
Luisgfx <luisfelipe.assuncao@hotmail.com>
2021-01-01 23:53:51 -0400

    Billboard Status (Thx @Upenha) and Help command

---
+++ src/commands/bot/HelpCommand.js
@@ -0,0 +1,51 @@
+const { Command, Embed } = require('../../structures')
+
+module.exports = class HelpCommand extends Command {
+  constructor (client) {
+    super({
+      name: 'help',
+      description: 'Sends my command list',
+      category: 'Bot',
+      options: [
+        {
+          name: 'command',
+          required: false,
+          description: 'Command to see more information',
+          type: 3
+        }
+      ]
+    }, client)
+  }
+
+  run (interaction) {
+    const embed = new Embed(interaction.user)
+    const command = interaction.args.length && this.client.commands.find(({ name }) => name === interaction.args[0].value)
+
+    if (command) {
+      embed
+        .setDescription([
+          `\`❓\` **Usage:** ${this.buildCommandUsage(command, interaction.prefix)}`,
+          `\`📜\` **Description:** \`${command.description}\``,
+          `\`🎫\` **Category:** \`${command.category}\``,
+          command.options.length ? '\n`💡` _**<>** are optional and **[]** are required_' : ''
+        ])
+    } else {
+      embed.setDescription([
+        `\`🌍\` **Prefix:** \`${interaction.prefix}\``,
+        '',
+        ...this.client.commands.map(({ name, description }) => `• \`/${name}\` - *${description}*`),
+        '',
+        `\`❓\` To know more about a command, use ${this.buildCommandUsage(this, interaction.prefix)}`
+      ])
+    }
+
+    interaction.send({
+      embeds: [embed]
+    })
+  }
+
+  buildCommandUsage (command, prefix) {
+    const formatOptions = (options) => options.map(option => option.required ? `[${option.name}]` : `<${option.name}>`).join(' ')
+    return `\`${prefix}${command.name}${command.options.length ? ` ${formatOptions(command.options)}` : ''}\``
+  }
+}
--- src/listeners/discord/ReadyListener.js
+++ src/listeners/discord/ReadyListener.js
@@ -1,6 +1,7 @@
 const { Listener } = require('../../structures')
+const moment = require('moment')
 const { Status } = require('../../../assets')
-// const axios = require('axios').default
+const billboard = require('billboard-top-100')
 
 const PRESENCE_INTERVAL = 60 * 1000
 
@@ -15,21 +16,22 @@ module.exports = class ReadyListener extends Listener {
   onReady () {
     this.client.user.setPresence({ activity: { name: `/help | ${this.client.user.tag}`, type: 'LISTENING' } })
 
-    /* axios.delete(`https://discord.com/api/v8/applications/${this.client.user.id}/guilds/791225121695858718/commands/792583074054930452`, {
-      headers: {
-        Authorization: `Bot ${this.client.token}`
-      }
-    })
-      .then(({ data }) => console.log(data))
-      .catch(e => console.log('Error')) */
-
-    setInterval(() => {
-      const activity = Status[Math.floor(Math.random() * Status.length)]
-      this.client.user.setPresence({ activity })
-    }, PRESENCE_INTERVAL)
+    setInterval(() => { this.updatePresence() }, PRESENCE_INTERVAL)
   }
 
   onDebug (data) {
     this.client.logger.debug(data)
+  }
+
+  updatePresence () {
+    billboard.getChart('hot-100', moment().format('YYYY-MM-DD'), (error, chart) => {
+      if (error) {
+        this.client.error(`Billboard system error. Using default songs, error: ${error}`)
+        const activity = Status[Math.floor(Math.random() * Status.length)]
+        this.client.user.setPresence({ activity })
+      }
+      const activity = chart.songs[Math.floor(Math.random() * chart.songs.length)]
+      this.client.user.setPresence({ activity: { name: `${activity.title} - ${activity.artist} | ${activity.rank}º on Billboard`, type: 'LISTENING' } })
+    })
   }
 }
